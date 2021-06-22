e244a14f443c0ec2550254e0c6464ab60ac8da8f
fumiee <fumie.yori@gmail.com>
2021-06-22 13:37:33 +0900

    ＃14までの全て

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import classes from "src/components/Header/Header.module.css";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <a className={classes.anchor}>Index </a>
+      </Link>
+      <Link href="/about">
+        <a className={classes.anchor}>About</a>
+      </Link>
+    </header>
+  );
+}
