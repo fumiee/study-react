30f5c7395eed7645c48f6ca2b694df4e7e06c860
fumiee <fumie.yori@gmail.com>
2021-08-27 15:52:21 +0900

    useRouterを使用 #22

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,9 +1,8 @@
+import Link from "next/link";
 import { usePosts } from "src/hooks/usePosts";
 
 export const Posts = () => {
   const { data, error, isLoading, isEmpty } = usePosts();
-
-  console.log({ data, error });
 
   if (isLoading) {
     return <div>ローディング中</div>;
@@ -18,7 +17,13 @@ export const Posts = () => {
   return (
     <ol>
       {data.map((post) => {
-        return <li key={post.id}>{post.title}</li>;
+        return (
+          <li key={post.id}>
+            <Link href={`/post/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        );
       })}
     </ol>
   );
